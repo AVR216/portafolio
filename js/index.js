cb208eaee5a188936a945a91221b47d3a4f9eeef
AVR216 <jhon.villalba.rubio@gmail.com>
2023-08-05 21:13:52 -0500

    btn download and about me section finished

--- js/index.js
+++ js/index.js
@@ -3,3 +3,9 @@ const btnNav = document.querySelector('.menu-btn');
 btnNav.addEventListener('click', ()=> {
     document.querySelector('.nav-menu').classList.toggle('show')
 });
+
+const btnDownloadCV = document.querySelector('.download_cv');
+btnDownloadCV.addEventListener('click', () => {
+    console.log("Download cv");
+    btnDownloadCV.setAttribute('href', './assets//files/cv.pdf');
+});
